89c1e615a13422aec8e148a65acd82cae7e91234
= <=>
2024-01-17 11:35:14 +0530

    added config to load api

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables
+
+def load_config(key: str) -> str:
+    """Loads a configuration value from environment variables."""
+    return os.environ.get(key)
